9ba57e062e093f6536d2a3ffaa5d7599bc7f779a
VanduFido <confidencevandu@gmail.com>
2022-04-30 21:59:50 +0100

    Add more comment to code

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import numpy as np
 
+## Create a dictionary mapping city name to the corresponding file name
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
